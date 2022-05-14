4f77a2646891684b2032700ac18eadb4246a21fc
Michael Trang <michael.trang@hotmail.com>
2022-05-14 16:07:03 +1000

    Initial Commit

---
+++ MTCoffeeMasters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MTCoffeeMasters
+//
+//  Created by Michael Trang on 14/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MTCoffeeMasters/MTCoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTCoffeeMastersApp.swift
+//  MTCoffeeMasters
+//
+//  Created by Michael Trang on 14/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct MTCoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
