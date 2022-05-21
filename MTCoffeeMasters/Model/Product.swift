668dcb5fd38775dc493e4e274757481a9be015e2
Michael Trang <michael.trang@hotmail.com>
2022-05-21 23:11:31 +1000

    add app launch screen

---
+++ MTCoffeeMasters/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  MTCoffeeMasters
+//
+//  Created by Michael Trang on 21/5/2022.
+//
+
+import Foundation
+
+struct Product: Decodable, Identifiable {
+    var id: Int
+    var name: String
+    var description: String
+    var price: Double
+    var image: String
+    
+    var imageURL: URL {
+        URL(string: "https://firtman.github.io/coffeemasters/api/images/\(self.image)")!
+    }
+}
